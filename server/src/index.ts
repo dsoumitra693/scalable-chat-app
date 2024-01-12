bd55963ac80d0db6571674e7577231bbf121f9e2
Soumitra Das (Sam) <dsoumitra693@gmail.com>
2024-01-12 13:39:48 +0530

    updating http server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import http from "http"
 import SocketServices from "./services/socket"
+require('dotenv').config();
 
 async function init() {
 
-  const httpServer = http.createServer();
+  const httpServer = http.createServer((req, res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Congrats you have a created a web server');
+    res.end();
+  });
+  
   const socketService = new SocketServices(httpServer)
-  const PORT = 3000
+  const PORT = process.env.PORT
 
   httpServer.listen(PORT, () => console.log(`HTTP server running on PORT ${PORT}`))
 
