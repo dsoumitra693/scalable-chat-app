6700ef5c39cc9f96d9e3783e651538aa97d7db7e
Soumitra Das (Sam) <dsoumitra693@gmail.com>
2024-01-14 23:15:46 +0530

    adding search bar to app

--- Frontend/components/Home/Header.tsx
+++ Frontend/components/Home/Header.tsx
@@ -1,6 +1,6 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
-import { Button, Divider, IconButton, Menu, useTheme } from 'react-native-paper'
+import { Button, Divider, IconButton, Menu, Searchbar, useTheme } from 'react-native-paper'
 import { useAuth } from '../../providers/AuthProvider'
 
 
@@ -9,22 +9,38 @@ const Header = () => {
   const { colors } = useTheme()
   const [visible, setVisible] = useState<boolean>()
   const { currentUser, logoutUser } = useAuth()
+  const [searchQuery, setSearchQuery] = useState<string>()
 
   const openMenu = () => setVisible(true)
   const closeMenu = () => setVisible(false)
+  const onChangeSearch = (query: string) => setSearchQuery(query)
 
   return (
     <View style={[styles.header, { backgroundColor: colors.background }]}>
-      <Text style={[styles.headerText, { color: colors.text }]}>Hello! {currentUser.name}</Text>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        paddingLeft: 10,
+      }}>
+        <Text style={[styles.headerText, { color: colors.text }]}>Hello! {currentUser.name}</Text>
 
-      <Menu
-        visible={visible}
-        onDismiss={closeMenu}
-        contentStyle={{ backgroundColor: colors.backdrop }}
-        anchor={<IconButton icon="menu" color={colors.primary} size={25} onPress={openMenu} />}>
-        <Menu.Item onPress={logoutUser} title="Logout" icon={'logout'} />
-        <Divider />
-      </Menu>
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          contentStyle={{ backgroundColor: colors.backdrop }}
+          anchor={<IconButton icon="menu" color={colors.primary} size={25} onPress={openMenu} />}>
+          <Menu.Item onPress={logoutUser} title="Logout" icon={'logout'} />
+          <Divider />
+        </Menu>
+      </View>
+      <Searchbar
+        placeholder="Find People"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{width:'95%', borderRadius:20}}
+      />
     </View>)
 }
 
@@ -32,14 +48,11 @@ export default Header
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingBottom: 30,
-    paddingLeft: 10,
-    maxHeight: 80,
     backgroundColor: 'transparent',
     top: statusbarHeight,
+    paddingBottom:35
   },
   headerText: {
     fontSize: 25,
